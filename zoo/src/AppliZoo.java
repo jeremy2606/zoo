f0f7a90d2648809947ec218df180cc754c6fb98d
jeremy2606 <84504373+jeremy2606@users.noreply.github.com>
2021-07-23 11:12:46 +0200

    merge effectué

--- zoo/src/AppliZoo.java
+++ zoo/src/AppliZoo.java
@@ -1,13 +1,35 @@
 import metier.AnimalManager;
 import metier.Manager;
 import model.Animal;
+import java.awt.EventQueue;
+
+import view.AppWindow;
+import view.DisplayUpdateWindow2;
 
 public class AppliZoo {
 
-    public static void main(String[] args) {
-        Manager<Animal> animalManager = new AnimalManager();
-        Animal animal = animalManager.getByName("Gianni");
-        System.out.println(animal);
-    }
-}
+	public static void main(String[] args) {
 
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppWindow frame = new AppWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DisplayUpdateWindow2 frame = new DisplayUpdateWindow2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
---
+++ zoo/src/view/ModelAnimal.java
@@ -0,0 +1,36 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import model.Animal;
+
+public class ModelAnimal extends AbstractTableModel {
+
+	private List<String> animalNameList;
+
+	public ModelAnimal() {
+		this.animalNameList = new ArrayList<>();
+	}
+
+	public ModelAnimal(List<Animal> animalList) {
+		for (Animal animal : animalList) {
+			animalNameList.add(animal.getName());
+		}
+	}
+
+	public int getColumnCount() {
+		return 1;
+	}
+
+	public int getRowCount() {
+		return this.animalNameList.size();
+	}
+
+	public Object getValueAt(int row, int col) {
+		return Integer.valueOf(row * col);
+	}
+
+}
