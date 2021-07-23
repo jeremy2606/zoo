df5e015118a6ef08ac57cd36aca4acb22a88ce36
Ay1224 <amal.ayed.ETU@gmail.com>
2021-07-23 16:26:06 +0200

    Dao Enclos

--- zoo/src/dbaccess/DbEnclos.java
+++ zoo/src/dbaccess/DbEnclos.java
@@ -1,10 +1,11 @@
 package dbaccess;
 
-import model.Enclos;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import model.Animal;
+import model.Enclos;
 
 public class DbEnclos implements EnclosInterface {
 
@@ -14,11 +15,47 @@ public class DbEnclos implements EnclosInterface {
         this.connection = connection;
     }
 
-    public Enclos get(int id) {
-
+    public ArrayList<Animal> getAnimalList(int id) {
+    	ResultSet result;
+    	Animal animal;
+    	ArrayList<Animal> listAnimal = new ArrayList<Animal>();
+    	String SQLString ="SELECT animal.name, type_enclos.name, type_animal.name, type_regime_alimentaire.name, animal.commentaires\n" +
+                "FROM animal\n" +
+                "JOIN type_animal\n" +
+                "ON animal.type_animal_id = type_animal.id\n" +
+                "JOIN type_regime_alimentaire\n" +
+                "ON type_animal.type_regime_alimentaire_id = type_regime_alimentaire.id\n" +
+                "JOIN enclos\n" +
+                "ON animal.enclos_id = enclos.id\n" +
+                "JOIN type_enclos\n" +
+                "ON enclos.type_enclos_id = type_enclos.id\n"+
+                "WHERE enclos.id = ?;";
+    	try (PreparedStatement getListStatement = connection.prepareStatement(SQLString)) {
+            getListStatement.setInt(1, id);
+            result = getListStatement.executeQuery();
+            while (result.next()) {
+                animal = new Animal(
+                        result.getString(1),
+                        result.getString(2),
+                        result.getString(3),
+                        result.getString(4),
+                        result.getString(5));
+                listAnimal.add(animal);
+               
+            }
+            result.close();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    	System.out.println(listAnimal);
+        return listAnimal;
+    	
+    }
+    public Enclos getById(int id) {
+    	
         Enclos enclos = null;
         ResultSet result;
-        String SQLString = "SELECT * FROM enclos WHERE id=?;";
+        String SQLString = "SELECT * FROM enclos where id= ?";
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(SQLString);
             preparedStatement.setInt(1, id);
@@ -31,15 +68,36 @@ public class DbEnclos implements EnclosInterface {
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
         }
+        System.out.println(enclos);
         return enclos;
     }
 
+	@Override
+	public Enclos getByName(String name) {
+		Enclos enclos = null;
+        ResultSet result;
+        String SQLString = "SELECT * FROM enclos where name= ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLString);
+            preparedStatement.setString(1, name);
+            result = preparedStatement.executeQuery();
+            if (result.next()) {
+                enclos = new Enclos(result.getInt(1), result.getString(2), result.getInt(3), result.getInt(4));
+            }
+            result.close();
+            preparedStatement.close();
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+        System.out.println(enclos);
+        return enclos;
+    }
     public List<Enclos> getAll() {
         List<Enclos> listEnclos = new ArrayList<>();
         try {
             Statement query;
             query = connection.createStatement();
-            ResultSet result = query.executeQuery("SELECT * FROM animal;");
+            ResultSet result = query.executeQuery("SELECT * FROM enclos");
             while (result.next()) {
                 listEnclos.add(new Enclos(result.getInt(1), result.getString(2), result.getInt(3), result.getInt(4)));
             }
@@ -48,6 +106,7 @@ public class DbEnclos implements EnclosInterface {
         } catch (SQLException se) {
             se.printStackTrace();
         }
+        System.out.println(listEnclos);
         return listEnclos;
     }
 
@@ -59,14 +118,16 @@ public class DbEnclos implements EnclosInterface {
             saveEnclos.setInt(2, enclos.getTypeEnclosId());
             saveEnclos.setInt(3, enclos.getZooId());
             saveEnclos.executeUpdate();
+            connection.setAutoCommit(false);
             connection.commit();
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
+        
     }
 
 
-    @Override
+	@Override
     public void update(Enclos enclos) {
         String SQLString = "UPDATE enclos SET name=?, type_enclos_id=?, zoo_id=? " +
                 "WHERE id = ?;";
@@ -93,5 +154,6 @@ public class DbEnclos implements EnclosInterface {
             sqlException.printStackTrace();
         }
     }
+
 
 }
--- zoo/src/dbaccess/EnclosInterface.java
+++ zoo/src/dbaccess/EnclosInterface.java
@@ -1,14 +1,19 @@
 package dbaccess;
 
+import model.Animal;
 import model.Enclos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface EnclosInterface {
 	
-	Enclos get(int id);
+	Enclos getById(int id);
+	Enclos getByName(String name);
+	ArrayList<Animal> getAnimalList(int id);
     List<Enclos> getAll();
+	void update(Enclos enclos);
     void save(Enclos enclos);
-    void update(Enclos enclos);
     void delete(Enclos enclos);
+
 }
