d753d387578e6b04fe527c33ab32526b20528fb1
Enrico Lo Faro <elofaro@outlook.fr>
2021-07-26 14:15:09 +0200

    :sparkle: add animal fonctionnality finished commit2

--- zoo/src/view/ModelAnimal.java
+++ zoo/src/view/ModelAnimal.java
@@ -16,7 +16,7 @@ public class ModelAnimal extends AbstractTableModel {
 	}
 
 	public ModelAnimal(List<Animal> animalList) {
-		this.animalNameList = new ArrayList<>();
+		animalList = new ArrayList<Animal>();
 		for (Animal animal : animalList) {
 			animalNameList.add(animal.getName());
 		}
