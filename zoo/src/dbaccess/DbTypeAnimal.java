39831d04e185446b1ffb1f3066feccc6466522f3
Enrico Lo Faro <elofaro@outlook.fr>
2021-07-26 09:42:22 +0200

    :construction: add new animal fonctionnality in progress

--- zoo/src/dbaccess/DbTypeAnimal.java
+++ zoo/src/dbaccess/DbTypeAnimal.java
@@ -2,61 +2,71 @@ package dbaccess;
 
 import model.TypeAnimal;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DbTypeAnimal implements TypeAnimalInterface {
-	List<TypeAnimal> listTypeAnimal = new ArrayList<TypeAnimal>();
-	static Statement query = null;
+    static Statement query = null;
+    List<TypeAnimal> listTypeAnimal = new ArrayList<TypeAnimal>();
+    private Connection connection;
 
-	private Connection c;
+    public DbTypeAnimal(Connection conne) {
+        this.connection = conne;
+    }
 
-	public DbTypeAnimal(Connection c) {
-		this.c = c;
-	}
+    public TypeAnimal get(int id) {
 
-	public TypeAnimal get(int id) {
+        TypeAnimal typeAnimal = null;
+        ResultSet result;
+        String SQLString = "SELECT * FROM type_animal WHERE id=?;";
+        try {
 
-		TypeAnimal typeAnimal = null;
-		ResultSet result;
-		String SQLString = "SELECT * FROM type_animal WHERE id=?;";
-		try {
+            PreparedStatement preparedStatement = this.connection.prepareStatement(SQLString);
+            preparedStatement.setInt(1, id);
 
-			PreparedStatement preparedStatement = this.c.prepareStatement(SQLString);
-			preparedStatement.setInt(1, id);
+            result = preparedStatement.executeQuery();
+            if (result.next()) {
+                typeAnimal = new TypeAnimal(result.getInt(1), result.getString(2), result.getInt(3));
+            }
+            result.close();
+            preparedStatement.close();
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return typeAnimal;
+    }
 
-			result = preparedStatement.executeQuery();
-			if (result.next()) {
-				typeAnimal = new TypeAnimal(result.getInt(1), result.getString(2),result.getInt(3));
-			}
-			result.close();
-			preparedStatement.close();
-		} catch (SQLException exception) {
-			System.out.println(exception.getMessage());
-		}
-		return typeAnimal;
-	}
+    public List<TypeAnimal> getAll() {
 
-	public List<TypeAnimal> getAll() {
+        try {
+            Statement query;
+            query = this.connection.createStatement();
+            ResultSet result = query.executeQuery("SELECT * FROM type_animal;");
+            while (result.next()) {
+                listTypeAnimal.add(new TypeAnimal(result.getInt(1), result.getString(2), result.getInt(3)));
+            }
+            result.close();
+            query.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+        return listTypeAnimal;
+    }
 
-		try {
-			Statement query;
-			query = this.c.createStatement();
-			ResultSet result = query.executeQuery("SELECT * FROM type_animal;");
-			while (result.next()) {
-				listTypeAnimal.add(new TypeAnimal(result.getInt(1), result.getString(2),result.getInt(3)));
-			}
-			result.close();
-			query.close();
-		} catch (SQLException se) {
-			se.printStackTrace();
-		}
-		return listTypeAnimal;
-	}
-
+    public TypeAnimal getByName(String name) {
+        TypeAnimal typeAnimal = null;
+        ResultSet result;
+        String SQLString = "SELECT * FROM type_animal WHERE name = ?;";
+        try (PreparedStatement getByName = connection.prepareStatement(SQLString)) {
+            getByName.setString(1, name);
+            result = getByName.executeQuery();
+            typeAnimal = new TypeAnimal(result.getInt(1), result.getString(2),
+                    result.getInt(3));
+            result.close();
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return typeAnimal;
+    }
 }
